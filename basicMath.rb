0d514c5ac4549d72c39ad7acba30fc58c970cc78
fongelias <fongelias@gmail.com>
2018-05-04 11:55:11 -0400

    added a script to illustrate basic math from docs

---
+++ basicMath.rb
@@ -0,0 +1,15 @@
+puts "Basic Math"
+puts "-"*10
+puts "addition, multiplication, subtraction: 3 + 2 - 1 * 2"
+puts 3+2-1*2
+puts "squaring: 3 ** 2"
+puts 3 ** 2
+puts "square root: 9"
+puts Math.sqrt(9)
+
+
+
+
+
+
+
